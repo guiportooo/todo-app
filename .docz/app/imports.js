16b78afb159932977f5da5e9b752a584dc0b81fc
Guilherme Porto <guiportooo@gmail.com>
2019-02-14 18:14:13 -0200

    Configure docz

--- .docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'src/components/AddButton/AddButton.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-add-button-add-button" */ 'src/components/AddButton/AddButton.mdx'),
-  'src/components/Buttons/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-buttons-button" */ 'src/components/Buttons/Button.mdx'),
-  'src/components/CreateButton/CreateButton.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-create-button-create-button" */ 'src/components/CreateButton/CreateButton.mdx'),
-}
