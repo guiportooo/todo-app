4d1bb69208329bf708b0fad1900260473aece64d
Guilherme Porto <guiportooo@gmail.com>
2019-02-17 21:55:11 -0300

    Clear initial App files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import ThemeWrapper from './components/ThemeWrapper.js';
+import { H1 } from './components/Typography';
 
 class App extends Component {
   render() {
     return (
       <ThemeWrapper>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <H1>Another Todo App!</H1>
       </ThemeWrapper>
     );
   }
