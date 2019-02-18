ee16f9482479a9f3087deabbe8752342e0533c97
Guilherme Porto <guiportooo@gmail.com>
2019-02-17 21:51:05 -0300

    Add reset styles to the GlobalStyle component

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
   @font-face {
     font-family: 'SF Pro Display';
     src: url('../fonts/SF-Pro-Display-Regular.otf');
@@ -9,6 +16,12 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'SF Pro Text';
     src: url('../fonts/SF-Pro-Text-Regular.otf');
+  }
+
+  h1,
+  h2,
+  h3 {
+    margin: 0;
   }
 `;
 
